d9088b38740784179e140b44c4645bbcd0a11c53
weiminghao <wei.mh@qq.com>
2025-11-22 21:05:35 +0800

    修改版本号

--- plugins/barkmultiusermsg/__init__.py
+++ plugins/barkmultiusermsg/__init__.py
@@ -16,7 +16,7 @@ class BarkMultiUserMsg(_PluginBase):
     # 插件图标
     plugin_icon = "Bark_A.png"
     # 插件版本
-    plugin_version = "1.4"
+    plugin_version = "1.0"
     # 插件作者
     plugin_author = "weimh"
     # 作者主页
